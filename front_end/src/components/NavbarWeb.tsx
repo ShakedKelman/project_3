2eb0a122ca704690d03f647d4b28fe7b5676592d
ShakedKelman <shaked.kelman@gmail.com>
2024-09-08 17:38:09 +0300

    changes home page to login and register- when logged or reg the vacations nav appears

--- front_end/src/components/NavbarWeb.tsx
+++ front_end/src/components/NavbarWeb.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom'; // Import Link for navigation
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store'; // Adjust import path as needed
 
 const NavbarWeb: React.FC = () => {
+    const authStatus = useSelector((state: RootState) => state.auth.status);
+
     return (
         <Navbar className="navbar-lilac" variant="light">
             <Container>
-                <Navbar.Brand as={Link} to="/">Home</Navbar.Brand> {/* Link to home or main page */}
+                <Navbar.Brand as={Link} to={authStatus === 'succeeded' ? "/" : "/login"}>
+                    {authStatus === 'succeeded' ? 'Home' : 'Login'}
+                </Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/register">Register</Nav.Link> {/* Link to Register page */}
-                    <Nav.Link as={Link} to="/login">Login</Nav.Link> {/* Link to Login page */}
-                    <Nav.Link as={Link} to="/vacations">Vacations</Nav.Link> {/* Link to Vacations page */}
+                    {authStatus === 'succeeded' && (
+                        <Nav.Link as={Link} to="/vacations">Vacations</Nav.Link> // Link to Vacations page
+                    )}
+                    {authStatus !== 'succeeded' && (
+                        <Nav.Link as={Link} to="/register">A new user?</Nav.Link> // Link to Register page
+                    )}
                 </Nav>
             </Container>
         </Navbar>
--- front_end/src/components/SiteRoutes.tsx
+++ front_end/src/components/SiteRoutes.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import VacationCard from './VacationCard'; // Adjust import path as needed
 import LoginComponent from './Login';
+import RegisterComponent from './Register';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
-import RegisterComponent from './Register';
 import AddVacationForm from './AddVactionForm';
 
 const SiteRoutes: React.FC = () => {
@@ -12,17 +12,22 @@ const SiteRoutes: React.FC = () => {
 
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/login" element={<LoginComponent />} />
-      {/* <Route path="/register" element={<RegisterComponent />} /> */}
       <Route path="/register" element={<RegisterComponent />} />
-      <Route path="/" element={<RegisterComponent />} />
-
-      <Route 
-        path="/vacations" 
-        element={authStatus === 'succeeded' ? <VacationCard /> : <Navigate to="/login" />} 
+      
+      {/* Redirect home to login page */}
+      <Route path="/" element={<Navigate to="/login" />} />
+      
+      {/* Protected Routes */}
+      <Route
+        path="/vacations"
+        element={authStatus === 'succeeded' ? <VacationCard /> : <Navigate to="/login" />}
       />
-                <Route path="/add-vacation" element={<AddVacationForm />} />
-
+      <Route
+        path="/add-vacation"
+        element={authStatus === 'succeeded' ? <AddVacationForm /> : <Navigate to="/login" />}
+      />
     </Routes>
   );
 };
